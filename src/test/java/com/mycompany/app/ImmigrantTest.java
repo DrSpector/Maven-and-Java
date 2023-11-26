a8a168945e5bdc66501b15075d5585fdd73cbc0d
Dle33 <lduyviet999@gmail.com>
2023-11-26 14:17:31 -0500

    Add to ImmigrantTest

--- src/test/java/com/mycompany/app/ImmigrantTest.java
+++ src/test/java/com/mycompany/app/ImmigrantTest.java
@@ -14,8 +14,8 @@ public class ImmigrantTest {
     @Before
     public void setUp() {
     	immigrant = new Immigrant("John", "Doe", "123456");
-	database.setImmigrant(immigrant);
-	database.setImmigrant(new Immigrant("Joe","Deck","0987654"));
+	    database.setImmigrant(immigrant);
+	    database.setImmigrant(new Immigrant("Joe","Deck","0987654"));
 
     }
 	
@@ -30,18 +30,25 @@ public class ImmigrantTest {
         assertEquals("John", immigrant.getFirstName());
         assertEquals("Doe", immigrant.getLastName());
         assertEquals("123456", immigrant.getAlienID());
+        //add NOV 25 DL
+        assertEquals("", immigrant.getStatus());
         
         immigrant3 = new Immigrant("Joe", "Deck", "0987654");
         assertEquals("Joe", immigrant3.getFirstName());
         assertEquals("Deck", immigrant3.getLastName());
         assertEquals("0987654", immigrant3.getAlienID());
-        
+        //add NOV 25 DL
+        assertEquals("", immigrant3.getStatus());
+
+        immigrant3 = new Immigrant(null, null, null);
+        assertEquals(null, immigrant3.getFirstName());
+        assertEquals(null, immigrant3.getLastName());
+        assertEquals(null, immigrant3.getAlienID());
+        assertEquals("", immigrant3.getStatus());
     }
 
 	@Test
     public void testCreateImmigrant1() {
-
-        
 
 	immigrant = Immigrant.createImmigrant1(null,"lorem","ipsum");
 	assertNull(immigrant);
@@ -52,14 +59,14 @@ public class ImmigrantTest {
 	immigrant = Immigrant.createImmigrant1("lorem","ipsum",null);
 	assertNull(immigrant);
 
-        immigrant3 = Immigrant.createImmigrant1("0987654", "Joe","Deck");
+    immigrant3 = Immigrant.createImmigrant1("0987654", "Joe","Deck");
 	assertNotNull(immigrant3);
-        assertEquals("Joe", immigrant3.getFirstName());
-        assertEquals("Deck", immigrant3.getLastName());
-        assertEquals("0987654", immigrant3.getAlienID());
+    assertEquals("Joe", immigrant3.getFirstName());
+    assertEquals("Deck", immigrant3.getLastName());
+    assertEquals("0987654", immigrant3.getAlienID());
         
     }
-
+    // Any of the field except status cannot be null.
     @Test
     public void testValidateInfo() {
         assertTrue(immigrant.validateInfo());
@@ -73,6 +80,15 @@ public class ImmigrantTest {
         
         immigrant = new Immigrant("John", "Doe", null);
         assertFalse(immigrant.validateInfo());
+
+        //add NOV 25 2023 DL
+
+        immigrant = new Immigrant(null, null, "12346");
+        assertFalse(immigrant.validateInfo());
+
+        immigrant = new Immigrant(null, null, null);
+        assertFalse(immigrant.validateInfo());
+        
     }
 
     @Test
@@ -87,8 +103,8 @@ public class ImmigrantTest {
 
     @Test
     public void testSetStatus() {
-        assertEquals(immigrant.getStatus(),""); // Status should be null initially
-	//assertEquals(immigrant3.getStatus(),"");
+        //status should be empty string intially
+        assertEquals(immigrant.getStatus(),"");
 		
 		
         immigrant.setStatus("Pending");
@@ -101,9 +117,16 @@ public class ImmigrantTest {
 	@Test
 	public void testEquals()
 	{
-
 		Immigrant immigrant4 = new Immigrant("John", "Doe", "123456");
-		assertTrue(immigrant4.equals(immigrant));
+		assertTrue(immigrant4.equals(new Immigrant("John", "Doe", "123456")));
+
+        //add NOV 25 DL
+        //Test two different immigrant.
+		assertFalse(immigrant4.equals(new Immigrant("Bob", "Doe", "123456")));
+
+        assertFalse(immigrant4.equals(new Immigrant("John", "Joe", "123456")));
+
+        assertFalse(immigrant4.equals(new Immigrant("John", "Doe", "123457")));
 	}
 
 }
